8a25fa30bee4eef9276a79f3612babfe7e01dcb7
42010 <72303709+42010@users.noreply.github.com>
2021-07-16 17:24:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function preload() {
 }
 
 function setup() {
-    canvas = createCanvas(600, 500);
+    canvas = createCanvas(400, 300);
     canvas.center();
 
     video = createCapture(VIDEO);
@@ -13,7 +13,7 @@ function setup() {
 }
 
 function draw() {
-    image(video, 0, 0, 600, 500);
+    image(video, 0, 0, 400, 300);
 }
 
 function play() {
